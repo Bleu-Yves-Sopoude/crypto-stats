f914a2fca97f834d65da786fabca3666cec7d342
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-08-22 10:35:56 +0000

    Add

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div>
+      <nav className="nav">
+        <h1 className="h1">Crypto currency</h1>
+      </nav>
+    </div>
+  );
+}
