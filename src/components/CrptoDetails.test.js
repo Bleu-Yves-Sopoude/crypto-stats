bc2ea95f00e43f00c06f91f38eca8f2a1ba9672b
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-08-24 17:13:54 +0000

    Fix linter

--- src/components/CrptoDetails.test.js
+++ src/components/CrptoDetails.test.js
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import configureStore from 'redux-mock-store';
-import CryptoDetails from '../components/CryptoDetails.js';
+import CryptoDetails from './CryptoDetails';
 import '@testing-library/jest-dom';
 
 const mockStore = configureStore([]);
